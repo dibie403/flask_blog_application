c224188eff1c1db0d40df1880d7890b551c73d53
dibie403 <152690737+dibie403@users.noreply.github.com>
2025-01-22 17:39:40 +0100

    more change to requirem.txt

--- config.py
+++ config.py
@@ -1,25 +1,34 @@
-import os 
+import os
 from dotenv import load_dotenv
 import firebase_admin
 from firebase_admin import credentials, storage
 
 class Config:
+    # Load environment variables
+    load_dotenv()
 
-		SECRET_KEY=(os.environ.get('SECRET_KEY'))
-		#SQLALCHEMY_DATABASE_URI =(os.environ.get('SQLALCHEMY_DATABASE_URI'))
-		SQLALCHEMY_DATABASE_URI =(os.environ.get('SQLALCHEMY_DATABASE_URI'))
-		SQLALCHEMY_TRACK_MODIFICATIONS = False
-		load_dotenv()
-		firebase_credentials_path = os.getenv("Firebase_Credentials")
+    # Application secret key
+    SECRET_KEY = os.getenv('SECRET_KEY')
 
-		if firebase_credentials_path:
-		    # Initialize Firebase Admin SDK
-		    cred = credentials.Certificate(firebase_credentials_path)
-		    firebase_admin.initialize_app(cred, {
-		        'storageBucket': 'face-check-attendance.appspot.com'  # Your Firebase Storage bucket name
-		    })
-		else:
-		    print("Firebase credentials path is missing.")
+    # Database URI
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Firebase setup
+    firebase_credentials_path = os.getenv("Firebase_Credentials")
+
+    if firebase_credentials_path:
+        try:
+            # Initialize Firebase Admin SDK
+            cred = credentials.Certificate(firebase_credentials_path)
+            firebase_admin.initialize_app(cred, {
+                'storageBucket': 'face-check-attendance.appspot.com'  # Replace with your bucket name
+            })
+        except Exception as e:
+            print(f"Error initializing Firebase Admin SDK: {e}")
+    else:
+        print("Firebase credentials path is missing.")
+
 
 		
 
